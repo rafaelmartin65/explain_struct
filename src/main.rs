e904acb91c24620a17d73b2b2501ba7577294223
Rafael Martin <rmartin65@gmail.com>
2023-06-24 16:50:01 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main(){
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    
+    println!("Print Area rectangle: {}", rect1.area());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    // if rect1.width() {
+    //     println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    // }else{
+    //     println!("The rectangle has a zero width; it is {}", rect1.width);
+    // }
+}
+
+// impl Rectangle {
+//     fn area(self: &Self) -> u32 {
+//         self.width * self.height
+//     }
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     println!(
+//         "The Area of the rectangle is  {} square pixels.",
+//         rect1.area()
+//     );
+
+// }
+
